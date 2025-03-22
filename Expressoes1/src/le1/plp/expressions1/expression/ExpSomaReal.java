6b0211c79edef3d63d87435b658acf633c79f29c
Heitor Carvalho <heitor.scz@gmail.com>
2025-03-22 20:06:42 -0300

    Adiciona soma para reais

---
+++ Expressoes1/src/le1/plp/expressions1/expression/ExpSomaReal.java
@@ -0,0 +1,61 @@
+package le1.plp.expressions1.expression;
+
+import le1.plp.expressions1.util.Tipo;
+import le1.plp.expressions1.util.TipoPrimitivo;
+import le1.plp.expressions2.memory.AmbienteCompilacao;
+import le1.plp.expressions2.memory.AmbienteExecucao;
+
+/**
+ * Um objeto desta classe representa uma Expressao de Soma.
+ */
+public class ExpSomaReal extends ExpBinaria {
+
+	/**
+	 * Controi uma Expressao de Soma com as sub-expressoes especificadas.
+	 * Assume-se que estas sub-expressoes resultam em <code>ValorReal</code> 
+	 * quando avaliadas.
+	 * @param esq Expressao da esquerda
+	 * @param dir Expressao da direita
+	 */
+	public ExpSomaReal(Expressao esq, Expressao dir) {
+		super(esq, dir, "+");
+	}
+
+	/**
+	 * Retorna o valor da Expressao de Soma
+	 * 
+	 * @param amb
+	 *            o ambiente de execu��o.
+	 */
+	public Valor avaliar(AmbienteExecucao amb) {
+		return new ValorReal(
+			((ValorReal) getEsq().avaliar(amb)).valor() +
+			((ValorReal) getDir().avaliar(amb)).valor() );
+	}
+	
+	/**
+	 * Realiza a verificacao de tipos desta expressao.
+	 *
+	 * @param amb
+	 *            o ambiente de compila��o.
+	 *
+	 * @return <code>true</code> se os tipos da expressao sao validos;
+	 *         <code>false</code> caso contrario.
+	 */
+	protected boolean checaTipoElementoTerminal(AmbienteCompilacao amb) {
+		return (getEsq().getTipo(amb).eReal() && getDir().getTipo(amb).eReal());
+	}
+
+	/**
+	 * Retorna os tipos possiveis desta expressao.
+	 * 
+	 * @param amb
+	 *            o ambiente de compila��o.
+	 * 
+	 * @return os tipos possiveis desta expressao.
+	 */
+	public Tipo getTipo(AmbienteCompilacao amb) {
+		return TipoPrimitivo.REAL;
+	}
+
+}
